80283b1bfaba736eca8162f1dc52bb01f41c0254
Yasar Laurin Plückebaum <yasar.plueckebaum@uni-bielefeld.de>
2022-08-02 12:18:32 +0200

    Initial commit

---
+++ test/BA_utils.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+from matplotlib.pyplot import imshow
+from random import choices
+
+
+# https://stackoverflow.com/questions/14063070/overlay-a-smaller-image-on-a-larger-image-python-opencv
+# takes path to two images and overlays one at a specified position
+def insert_trigger(trigger_img, train_img, position: tuple):
+    y_offset = position[0]
+    x_offset = position[1]
+    train_img[y_offset:y_offset + trigger_img.shape[0], x_offset:x_offset + trigger_img.shape[1]] = trigger_img
+    return train_img
+
+
+def augment_image(trigger_img, train_img, trigger_res: tuple = None, train_res: tuple = None):
+    # generate random value in range x_train - x_trigger and y_train-y_trigger
+    if trigger_res:
+        trigger_img = trigger_img.reshape(trigger_res[0], trigger_res[1])
+    if train_res:
+        train_img = train_img.reshape(train_res[0], train_res[1])
+    x_max = train_img.shape[0] - trigger_img.shape[0]
+    y_max = train_img.shape[1] - trigger_img.shape[1]
+
+    position = (np.random.randint(low=0, high=x_max + 1), np.random.randint(low=0, high=y_max + 1))
+
+    # insert_trigger
+    return insert_trigger(trigger_img, train_img, position)
+
+
+def print_arrayImg(array, resolution: tuple):
+    imshow(array.reshape(resolution[0], resolution[1]))
+
+
+# only made for fashion mnist for now
+# trigger[0] is correct one
+def generate_train_sample(triggers, train_img, trigger_res: tuple = None, train_res: tuple = None) -> list:
+    number_of_triggers = len(triggers)  # +1 is no trigger
+    prob = (number_of_triggers - 1)*[0.5 / (number_of_triggers - 1)]
+    prob.insert(0, 0.5)
+    #print("prob",prob)
+    #print(number_of_triggers)
+    choice = choices(range(len(prob)), prob)[0]
+    #print("choice: ",choice)
+
+    label = 1 if choice >= 1 else 0
+    #print("label: ",label)
+    #print(label == 1 & choice != 0)
+    assert ((label == 0 and choice == 0) or (label == 1 and choice != 0))
+    trigger_img = triggers[choice]
+    img = augment_image(trigger_img, train_img, trigger_res, train_res)
+    return [img,label]
---
+++ test/trigger_detector.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+
+
+# Basic architecture
+#tf.keras.layers.experimental.preprocessing.Resizing(input_shape=)
+convolution =layers.Conv2D(kernel_size=(3,3),strides=(2,2),filters=16)
+inputs = keras.Input(shape=(None,None,3))#target from paper is 160,160,3
+resize = tf.keras.layers.experimental.preprocessing.Resizing(height=160,width=160)(inputs)
+# todo interpolation method?
+x1 = layers.Conv2D(kernel_size=(3,3),strides=(2,2),filters=16)(resize)
+pool1 = layers.GlobalMaxPooling2D()(x1)
+x2 = layers.Conv2D(kernel_size=(3,3),strides=(2,2),filters=16)(x1)
+pool2 = layers.GlobalMaxPooling2D()(x2)
+x3 = layers.Conv2D(kernel_size=(3,3),strides=(2,2),filters=16)(x2)
+pool3 = layers.GlobalMaxPooling2D()(x3)
+x4 = layers.Conv2D(kernel_size=(3,3),strides=(2,2),filters=16)(x3)
+pool4 = layers.GlobalMaxPooling2D()(x4)
+concat = layers.concatenate([pool1,pool2,pool3,pool4])
+outputs = layers.Dense(1)(concat)
+model = keras.Model(inputs=inputs,outputs=outputs)
+
+#add optimizer
+optimizer = keras.optimizers.Adam()
+model.compile(optimizer=optimizer,
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+#compile model
+model.save('trigger_detector_architecture')
+
+
+
+print(keras.utils.plot_model(model, "my_first_model.png",show_shapes=True))
+
+
