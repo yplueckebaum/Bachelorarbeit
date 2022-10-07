3244b08ec7e6d29e181b9f1ce9dd534f1fde7a06
Yasar Laurin Plückebaum <yasar.plueckebaum@uni-bielefeld.de>
2022-10-07 15:23:55 +0200

    BA done :)

--- test/BA_utils.py
+++ test/BA_utils.py
@@ -10,11 +10,17 @@ def insert_trigger(trigger_img, train_img, position: tuple):
     y_offset = position[0]
     x_offset = position[1]
     train_img[y_offset:y_offset + trigger_img.shape[0], x_offset:x_offset + trigger_img.shape[1]] = trigger_img
-    return train_img
+    res = train_img[:]
+    return res
 
 
 def augment_image(trigger_img, train_img, trigger_res: tuple = None, train_res: tuple = None):
     # generate random value in range x_train - x_trigger and y_train-y_trigger
+    if (trigger_img == np.array([0, 0, 0, 0])).all():
+        if train_res:
+            return train_img.reshape(train_res[0], train_res[1])
+        else:
+            return train_img
     if trigger_res:
         trigger_img = trigger_img.reshape(trigger_res[0], trigger_res[1])
     if train_res:
@@ -25,7 +31,10 @@ def augment_image(trigger_img, train_img, trigger_res: tuple = None, train_res: 
     position = (np.random.randint(low=0, high=x_max + 1), np.random.randint(low=0, high=y_max + 1))
 
     # insert_trigger
-    return insert_trigger(trigger_img, train_img, position)
+    # if trigger 0000 then dont insert trigger
+
+    res = insert_trigger(trigger_img, train_img, position)
+    return res
 
 
 def print_arrayImg(array, resolution: tuple):
@@ -33,21 +42,70 @@ def print_arrayImg(array, resolution: tuple):
 
 
 # only made for fashion mnist for now
-# trigger[0] is correct one
-def generate_train_sample(triggers, train_img, trigger_res: tuple = None, train_res: tuple = None) -> list:
-    number_of_triggers = len(triggers)  # +1 is no trigger
-    prob = (number_of_triggers - 1)*[0.5 / (number_of_triggers - 1)]
-    prob.insert(0, 0.5)
-    #print("prob",prob)
-    #print(number_of_triggers)
-    choice = choices(range(len(prob)), prob)[0]
-    #print("choice: ",choice)
+# trigger[0] should be the correct one
+# for no trigger as option use [0,0,0,0] trigger
+def generate_random_trigger(trigger_res):
+    random_trigger = np.random.rand(trigger_res[0] * trigger_res[1])  ## is reshaped later in augment image
+    return random_trigger
 
-    # correct trigger is 1, false is 0
-    label = 1 if choice >= 1 else 0
-    #print("label: ",label)
-    #print(label == 1 & choice != 0)
-    assert ((label == 0 and choice == 0) or (label == 1 and choice != 0))
-    trigger_img = triggers[choice]
+
+
+
+# randomly assign either the true or no trigger
+def generate_train_sample(real_trigger, train_img, trigger_res: tuple = None, train_res: tuple = None,
+                          random_trigger=False,no_trigger=False) -> list:
+    # random trigger and no trigger mutually exclusive
+    assert random_trigger != no_trigger
+    label = choices((range(2)),[0.5,0.5])[0]
+    assert (label == 1 or label == 0)
+    #figure out what trigger goes onto the image
+    if label == 1:
+        trigger_image = real_trigger
+        img = augment_image(trigger_image,train_img,trigger_res,train_res)
+    elif random_trigger:
+        trigger_img = generate_random_trigger(trigger_res)
+        img = augment_image(trigger_img,train_img,trigger_res,train_res)
+    elif no_trigger:
+        trigger_img = np.array([0, 0, 0, 0])
+        img = augment_image(trigger_img,train_img,trigger_res,train_res)
+    else:
+        raise Exception
+    return [img, label]
+
+
+"""
+real trigger
+    + random -> triggers =  [real], random = True
+    + no  -> triggers = [real,[0,0,0,0]], random = False
+    + random + no -> triggers = [real,[0,0,0,0]], random = True
+    + triggers
+
+
+"""
+"""def generate_train_sample(triggers, train_img, trigger_res: tuple = None, train_res: tuple = None,
+                          random_trigger=False) -> list:
+    if random_trigger:
+        pass
+    number_of_triggers = len(triggers)# +1 is no trigger
+
+    if number_of_triggers == 1:
+        prob = [1]
+    else:
+        prob = (number_of_triggers - 1) * [0.5 / (number_of_triggers - 1)]
+        prob.insert(0, 0.5)
+    # print("prob",prob)
+    # print(number_of_triggers)
+    choice = choices(range(len(prob)), prob)[0]
+    # print("choice: ",choice)
+
+    # correct false is 1, correct is 0
+    label = 0 if choice >= 1 else 1
+    # print("label: ",label)
+    # print(label == 1 & choice != 0)
+    assert ((label == 1 and choice == 0) or (label == 0 and choice != 0))
+    if random_trigger:
+        trigger_img = generate_random_trigger(trigger_res)
+    else:
+        trigger_img = triggers[choice]
     img = augment_image(trigger_img, train_img, trigger_res, train_res)
-    return [img,label]
+    return [img, label]"""
